b01babb9edfe800f16aca76479d427d3371d53f2
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2024-11-04 00:25:17 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ import { dailySendPhoto } from './service/dailySendPhoto';
 })();
 
 (() => {
-  const task = cron.schedule('25 0 * * *', () => {
+  const task = cron.schedule('26 0 * * *', () => {
     console.log('Starting daily sendPhoto...');
     dailySendPhoto();  // 调用下载任务的逻辑
   });
