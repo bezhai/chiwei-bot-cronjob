f28d9b6529bc3536c1df9b23f945aa6ea0d5ac4a
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2024-11-04 00:02:47 +0800

    update

--- src/service/dailySendPhoto.ts
+++ src/service/dailySendPhoto.ts
@@ -1,1 +1,14 @@
-export const dailySendPhoto = async (): Promise<void> => {};
+import dayjs from "dayjs";
+import { getPixivImages } from "../pixiv/pixivProxy";
+import { StatusMode } from "../pixiv/types";
+
+export const dailySendPhoto = async (): Promise<void> => {
+    const images = await getPixivImages({
+        status: StatusMode.NOT_DELETE,
+        page: 1,
+        page_size: 6,
+        random_mode: true,
+        start_time: dayjs().add(-1, 'day').valueOf(),
+    });
+    console.log(`有 ${images.length} 张图片可以发送`);
+};
