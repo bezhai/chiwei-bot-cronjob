9a9ad02e92717311edbd2fe5f61ccb96ea2513ae
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2024-11-08 19:23:53 +0800

    把发图时间调整到晚上7点半

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ import { dailySendPhoto } from './service/dailySendPhoto';
 })();
 
 (() => {
-  const task = cron.schedule('35 20 * * *', () => {
+  const task = cron.schedule('30 19 * * *', () => {
     console.log('Starting daily sendPhoto...');
     dailySendPhoto();  // 调用下载任务的逻辑
   });
