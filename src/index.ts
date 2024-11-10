af0c303710bbcdba431bca0cd2580826d90cb0e8
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2024-11-10 12:19:47 +0800

    更新时间，优化代码

--- src/index.ts
+++ src/index.ts
@@ -7,43 +7,27 @@ import { consumeDownloadTaskAsync } from './service/consumeService';
 import { mongoInitPromise } from './mongo/client';
 import { dailySendNewPhoto, sendDailyPhoto } from './service/dailySendPhoto';
 
-// 定义并启动定时任务
-(() => {
-  const task = cron.schedule('0 10 * * *', () => {
-    console.log('Starting download task...');
-    startDownload();  // 调用下载任务的逻辑
+// 抽象定时任务启动函数
+const scheduleTask = (cronTime: string, taskName: string, taskFn: () => void) => {
+  const task = cron.schedule(cronTime, () => {
+    console.log(`Starting ${taskName}...`);
+    taskFn();
   });
 
-  // 启动定时任务
   task.start();
+  console.log(`Cron job scheduled for ${taskName} at ${cronTime}.`);
+};
 
-  console.log('Cron job scheduled for midnight every day.');
-})();
+// 定时任务：下载任务
+scheduleTask('0 10 * * *', 'download task', startDownload);
 
-(() => {
-  const task = cron.schedule('16 12 * * *', () => {
-    console.log('Starting daily sendPhoto...');
-    sendDailyPhoto();  // 调用下载任务的逻辑
-  });
+// 定时任务：发送每日照片
+scheduleTask('0 18 * * *', 'daily sendPhoto', sendDailyPhoto);
 
-  // 启动定时任务
-  task.start();
+// 定时任务：发送每日新照片
+scheduleTask('30 19 * * *', 'daily sendNewPhoto', dailySendNewPhoto);
 
-  console.log('Cron job scheduled for midnight every day.');
-})();
-
-(() => {
-  const task = cron.schedule('30 19 * * *', () => {
-    console.log('Starting daily sendNewPhoto...');
-    dailySendNewPhoto();  // 调用下载任务的逻辑
-  });
-
-  // 启动定时任务
-  task.start();
-
-  console.log('Cron job scheduled for midnight every day.');
-})();
-
+// 异步消费任务
 (async () => {
   await mongoInitPromise;  // 等待 MongoDB 初始化完成
   try {
